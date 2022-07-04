451d26516a01b0fb30e36e070681229608a285e5
kodapassey <kodapassey@charter.net>
2022-07-04 14:22:05 -0700

    first question inserted

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+return inquirer.prompt([
+
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of your project?'
+    }
+
+])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
