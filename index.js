a4c67e8f20f777f3512712ef2a326c207c1d1e45
kodapassey <kodapassey@charter.net>
2022-07-04 14:32:17 -0700

    surrounded questions with a function and added the rest of the packages needed for the project

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 
 // TODO: Create an array of questions for user input
-return inquirer.prompt([
+function promptQuestions() {
+    return inquirer.prompt([
 
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'What is the name of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Give a brief description of your project: '
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Describe the installation process if any: '
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is the usage of this project?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose the correct license for this project: ',
-        choices: [
-            'Apache',
-            'Academic',
-            'GNU',
-            'ISC',
-            'MIT',
-            'Mozilla',
-            'Open'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'contributors',
-        message: 'Who are the contributors of this project?'
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Is there a test included?'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'What do I do if I have an issue? '
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Please enter your github username: '
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email: '
-    }
-])
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a brief description of your project: '
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe the installation process if any: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage of this project?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose the correct license for this project: ',
+            choices: [
+                'Apache',
+                'Academic',
+                'GNU',
+                'ISC',
+                'MIT',
+                'Mozilla',
+                'Open'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who are the contributors of this project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Is there a test included?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What do I do if I have an issue? '
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your github username: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email: '
+        }
+    ]);
+}
 
     .then((answers) => {
-        // Use user feedback for... whatever!!
-    })
+    // Use user feedback for... whatever!!
+})
     .catch((error) => {
         if (error.isTtyError) {
             // Prompt couldn't be rendered in the current environment
