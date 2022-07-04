7a3254ed1a1a85d9f6681675233a64723cac05be
kodapassey <kodapassey@charter.net>
2022-07-04 14:30:15 -0700

    added all questions into inquirer format

--- index.js
+++ index.js
@@ -10,9 +10,63 @@ return inquirer.prompt([
         type: 'input',
         name: 'projectTitle',
         message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief description of your project: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the installation process if any: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage of this project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the correct license for this project: ',
+        choices: [
+            'Apache',
+            'Academic',
+            'GNU',
+            'ISC',
+            'MIT',
+            'Mozilla',
+            'Open'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors of this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Is there a test included?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'What do I do if I have an issue? '
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your github username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email: '
     }
-
 ])
+
     .then((answers) => {
         // Use user feedback for... whatever!!
     })
