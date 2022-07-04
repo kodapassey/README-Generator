71464c1871fbb114a69d5a57a6b2018a3772d48b
kodapassey <kodapassey@charter.net>
2022-07-04 14:41:54 -0700

    beginning work on generateMarkdown.js page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
+const generateMarkdown = require("./utils/generateMarkdown")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
